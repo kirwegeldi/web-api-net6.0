9371ea9d0620c3c1c483f7142f42afd44fc98669
Serif Aksoy <aksoycoding@gmail.com>
2023-03-03 16:04:24 +0300

    inital project

---
+++ HPlusSport/Controllers/ProductsController.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace HPlusSport.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetProducts()
+        {
+            return Ok("ggwpeasy");
+        }
+    }
+}
